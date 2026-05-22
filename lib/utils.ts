d63ab22a1fd2d341cb871213851ed30c1e5e7872
Simen Andre Vikestrand Skogum <v0nNemizez@gmail.com>
2026-05-22 10:05:29 +0200

    Fix: Add cn function to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+// lib/utils.ts
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
+/**
+ * Combines Tailwind CSS classes with clsx for conditional class merging.
+ * @param inputs - Class values to merge.
+ * @returns Merged class string.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
