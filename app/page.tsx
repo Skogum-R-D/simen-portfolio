38c80b94583d572d15d1416464ecf7f81c93252f
Simen Andre Vikestrand Skogum <v0nNemizez@gmail.com>
2026-05-22 09:51:17 +0200

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
 import Hero from "@/components/hero";
-import About from "@/components/about";
-import Skills from "@/components/skills";
-import Projects from "@/components/projects";
-import Contact from "@/components/contact";
-import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#0a0a0f] text-white antialiased">
-      <Navbar />
+    <main>
       <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
     </main>
   );
 }
