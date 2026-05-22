e1e23a7270fbf9e63e4683b97e67677c0971a9d8
Simen Andre Vikestrand Skogum <v0nNemizez@gmail.com>
2026-05-22 09:27:39 +0200

    chore: add root layout with dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <body className={`${inter.className} bg-background text-foreground`}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
